f080cfc6f8a3a82866e2e00762a02219b1b0240f
Artur Kulig <arturkulig@gmail.com>
2018-07-10 15:33:23 +0200

    subject will drop subscription if all subsequent subscriptions are dropped as well

--- observable/subject.ts
+++ observable/subject.ts
@@ -1,29 +1,32 @@
 import { AsyncObserver } from "./observableTypes";
-import { subscribe } from "../observable/subscribe";
+import { subscribe, Subscription } from "../observable/subscribe";
 import { observable } from "../observable/observable";
 import { queue } from "./queue";
 
 export function subject<T, ERR = Error>(origin: AsyncIterable<T>) {
   const innerObservers: Array<AsyncObserver<T, ERR>> = [];
   let originLive = false;
+  let sub: Subscription | null = null;
 
   return observable<T, ERR>(
     queue(observer => {
       innerObservers.push(observer);
 
       if (!originLive) {
-        subscribe(origin, {
+        sub = subscribe(origin, {
           async next(value: T) {
             await Promise.all(
               innerObservers.map(observer => observer.next(value))
             );
           },
           async error(error: ERR) {
+            originLive = false;
             await Promise.all(
               innerObservers.splice(0).map(observer => observer.error(error))
             );
           },
           async complete() {
+            originLive = false;
             await Promise.all(
               innerObservers.splice(0).map(observer => observer.complete())
             );
@@ -34,6 +37,12 @@ export function subject<T, ERR = Error>(origin: AsyncIterable<T>) {
 
       return () => {
         innerObservers.splice(innerObservers.indexOf(observer), 1);
+        if (innerObservers.length === 0) {
+          originLive = false;
+          if (sub && !sub.closed) {
+            sub.unsubscribe();
+          }
+        }
       };
     })
   );
