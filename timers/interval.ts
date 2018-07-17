8f4ad1e9ea5a5c13507750ea06b8cb071aad8f69
Artur Kulig <arturkulig@gmail.com>
2018-07-17 13:36:40 +0200

    actually implement interval

--- timers/interval.ts
+++ timers/interval.ts
@@ -1,6 +1,16 @@
-import { timeout } from "../timers/timeout";
+import { observable } from "../core/observable";
+import { queue } from "../mods/queue";
 
-export async function* interval(timeInMs: number) {
-  await timeout(timeInMs);
-  yield Date.now();
+export function interval(timeInMs: number) {
+  return observable<number>(
+    queue(observer => {
+      observer.next(Date.now());
+      const token = setInterval(() => {
+        observer.next(Date.now());
+      }, timeInMs);
+      return () => {
+        clearInterval(token);
+      };
+    })
+  );
 }
