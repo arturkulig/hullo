672fb0de89c690f6b548cdd48f328adb81c2b3be
Artur Kulig <arturkulig@gmail.com>
2018-08-23 22:41:28 +0200

    cumulate operator

---
+++ op/cumulate.ts
@@ -0,0 +1,26 @@
+import { observable, subscribe } from "../core";
+
+export function* cumulate<T>(source: Iterable<T>) {
+  let lastCumulation: T[] = [];
+  for (const item of source) {
+    yield (lastCumulation = lastCumulation.concat(item));
+  }
+}
+
+export function cumulate$<T>(source: AsyncIterable<T>) {
+  return observable<T[]>(observer => {
+    let lastCumulation: T[] = [];
+    const sub = subscribe(source, {
+      next(item) {
+        return observer.next((lastCumulation = lastCumulation.concat(item)));
+      },
+      error: observer.error,
+      complete: observer.complete
+    });
+    return () => {
+      if (!sub.closed) {
+        sub.unsubscribe();
+      }
+    };
+  });
+}
