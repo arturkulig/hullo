b60dead23b01f84667f7e44b509d1cfbb1dcf44b
Artur Kulig <arturkulig@gmail.com>
2019-02-21 16:40:21 +0100

    make interval send initial value immediately

--- stream/interval.ts
+++ stream/interval.ts
@@ -4,11 +4,19 @@ import { buffer } from "./buffer";
 export const interval = (span: number) =>
   buffer(
     observable<number>(observer => {
-      const token = setInterval(() => {
-        observer.next(Date.now());
-      }, span);
+      let cancel = () => {};
+
+      cancel = observer.next(Date.now())(() => {
+        const token = setInterval(() => {
+          observer.next(Date.now());
+        }, span);
+        cancel = () => {
+          clearInterval(token);
+        };
+      });
+
       return () => {
-        clearInterval(token);
+        cancel();
       };
     })
   );
