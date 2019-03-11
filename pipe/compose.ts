ada621356e570714f19c66f6e343b42ffb693541
Artur Kulig <arturkulig@gmail.com>
2019-03-12 00:35:32 +0100

    pipe submodule tidying

--- pipe/compose.ts
+++ pipe/compose.ts
@@ -1,78 +1,80 @@
-export type Transformer<IN, R> = (input: IN, ...args: any[]) => R;
+export type Transformation<IN, R> = (input: IN, ...args: any[]) => R;
 
 export function compose<IN, R1, R2, R3, R4, R5, R6, R7, R8, R9>(
-  trans1: Transformer<IN, R1>,
-  trans2: Transformer<R1, R2>,
-  trans3: Transformer<R2, R3>,
-  trans4: Transformer<R3, R4>,
-  trans5: Transformer<R4, R5>,
-  trans6: Transformer<R5, R6>,
-  trans7: Transformer<R6, R7>,
-  trans8: Transformer<R7, R8>,
-  trans9: Transformer<R8, R9>
-): Transformer<IN, R9>;
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>,
+  transformation6: Transformation<R5, R6>,
+  transformation7: Transformation<R6, R7>,
+  transformation8: Transformation<R7, R8>,
+  transformation9: Transformation<R8, R9>
+): Transformation<IN, R9>;
 export function compose<IN, R1, R2, R3, R4, R5, R6, R7, R8>(
-  trans1: Transformer<IN, R1>,
-  trans2: Transformer<R1, R2>,
-  trans3: Transformer<R2, R3>,
-  trans4: Transformer<R3, R4>,
-  trans5: Transformer<R4, R5>,
-  trans6: Transformer<R5, R6>,
-  trans7: Transformer<R6, R7>,
-  trans8: Transformer<R7, R8>
-): Transformer<IN, R8>;
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>,
+  transformation6: Transformation<R5, R6>,
+  transformation7: Transformation<R6, R7>,
+  transformation8: Transformation<R7, R8>
+): Transformation<IN, R8>;
 export function compose<IN, R1, R2, R3, R4, R5, R6, R7>(
-  trans1: Transformer<IN, R1>,
-  trans2: Transformer<R1, R2>,
-  trans3: Transformer<R2, R3>,
-  trans4: Transformer<R3, R4>,
-  trans5: Transformer<R4, R5>,
-  trans6: Transformer<R5, R6>,
-  trans7: Transformer<R6, R7>
-): Transformer<IN, R7>;
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>,
+  transformation6: Transformation<R5, R6>,
+  transformation7: Transformation<R6, R7>
+): Transformation<IN, R7>;
 export function compose<IN, R1, R2, R3, R4, R5, R6>(
-  trans1: Transformer<IN, R1>,
-  trans2: Transformer<R1, R2>,
-  trans3: Transformer<R2, R3>,
-  trans4: Transformer<R3, R4>,
-  trans5: Transformer<R4, R5>,
-  trans6: Transformer<R5, R6>
-): Transformer<IN, R6>;
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>,
+  transformation6: Transformation<R5, R6>
+): Transformation<IN, R6>;
 export function compose<IN, R1, R2, R3, R4, R5>(
-  trans1: Transformer<IN, R1>,
-  trans2: Transformer<R1, R2>,
-  trans3: Transformer<R2, R3>,
-  trans4: Transformer<R3, R4>,
-  trans5: Transformer<R4, R5>
-): Transformer<IN, R5>;
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>
+): Transformation<IN, R5>;
 export function compose<IN, R1, R2, R3, R4>(
-  trans1: Transformer<IN, R1>,
-  trans2: Transformer<R1, R2>,
-  trans3: Transformer<R2, R3>,
-  trans4: Transformer<R3, R4>
-): Transformer<IN, R4>;
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>
+): Transformation<IN, R4>;
 export function compose<IN, R1, R2, R3>(
-  trans1: Transformer<IN, R1>,
-  trans2: Transformer<R1, R2>,
-  trans3: Transformer<R2, R3>
-): Transformer<IN, R3>;
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>
+): Transformation<IN, R3>;
 export function compose<IN, R1, R2>(
-  trans1: Transformer<IN, R1>,
-  trans2: Transformer<R1, R2>
-): Transformer<IN, R2>;
-export function compose<IN, R1>(xd1: Transformer<IN, R1>): Transformer<IN, R1>;
-export function compose<IN>(): Transformer<IN, IN>;
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>
+): Transformation<IN, R2>;
+export function compose<IN, R1>(
+  xd1: Transformation<IN, R1>
+): Transformation<IN, R1>;
+export function compose<IN>(): Transformation<IN, IN>;
 export function compose<IN, R>(
-  ...transformers: Transformer<any, any>[]
-): Transformer<IN, R>;
+  ...transformations: Transformation<any, any>[]
+): Transformation<IN, R>;
 
 export function compose(
-  ...transformers: Array<(input: any) => any>
+  ...transformations: Array<(input: any) => any>
 ): (input: any) => any {
-  return input => {
+  return function compose_I(input) {
     let result = input;
-    for (const transformer of transformers) {
-      result = (transformer as Function)(result);
+    for (const transformation of transformations) {
+      result = transformation(result);
     }
     return result;
   };
--- pipe/pipe.ts
+++ pipe/pipe.ts
@@ -1,80 +1,84 @@
-import { compose, Transformer } from "./compose";
+import { Transformation } from "./compose";
 
 export function pipe<IN, R1, R2, R3, R4, R5, R6, R7, R8, R9>(
   input: IN,
-  xd1: Transformer<IN, R1>,
-  xd2: Transformer<R1, R2>,
-  xd3: Transformer<R2, R3>,
-  xd4: Transformer<R3, R4>,
-  xd5: Transformer<R4, R5>,
-  xd6: Transformer<R5, R6>,
-  xd7: Transformer<R6, R7>,
-  xd8: Transformer<R7, R8>,
-  xd9: Transformer<R8, R9>
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>,
+  transformation6: Transformation<R5, R6>,
+  transformation7: Transformation<R6, R7>,
+  transformation8: Transformation<R7, R8>,
+  transformation9: Transformation<R8, R9>
 ): R9;
 export function pipe<IN, R1, R2, R3, R4, R5, R6, R7, R8>(
   input: IN,
-  xd1: Transformer<IN, R1>,
-  xd2: Transformer<R1, R2>,
-  xd3: Transformer<R2, R3>,
-  xd4: Transformer<R3, R4>,
-  xd5: Transformer<R4, R5>,
-  xd6: Transformer<R5, R6>,
-  xd7: Transformer<R6, R7>,
-  xd8: Transformer<R7, R8>
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>,
+  transformation6: Transformation<R5, R6>,
+  transformation7: Transformation<R6, R7>,
+  transformation8: Transformation<R7, R8>
 ): R8;
 export function pipe<IN, R1, R2, R3, R4, R5, R6, R7>(
   input: IN,
-  xd1: Transformer<IN, R1>,
-  xd2: Transformer<R1, R2>,
-  xd3: Transformer<R2, R3>,
-  xd4: Transformer<R3, R4>,
-  xd5: Transformer<R4, R5>,
-  xd6: Transformer<R5, R6>,
-  xd7: Transformer<R6, R7>
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>,
+  transformation6: Transformation<R5, R6>,
+  transformation7: Transformation<R6, R7>
 ): R7;
 export function pipe<IN, R1, R2, R3, R4, R5, R6>(
   input: IN,
-  xd1: Transformer<IN, R1>,
-  xd2: Transformer<R1, R2>,
-  xd3: Transformer<R2, R3>,
-  xd4: Transformer<R3, R4>,
-  xd5: Transformer<R4, R5>,
-  xd6: Transformer<R5, R6>
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>,
+  transformation6: Transformation<R5, R6>
 ): R6;
 export function pipe<IN, R1, R2, R3, R4, R5>(
   input: IN,
-  xd1: Transformer<IN, R1>,
-  xd2: Transformer<R1, R2>,
-  xd3: Transformer<R2, R3>,
-  xd4: Transformer<R3, R4>,
-  xd5: Transformer<R4, R5>
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>,
+  transformation5: Transformation<R4, R5>
 ): R5;
 export function pipe<IN, R1, R2, R3, R4>(
   input: IN,
-  xd1: Transformer<IN, R1>,
-  xd2: Transformer<R1, R2>,
-  xd3: Transformer<R2, R3>,
-  xd4: Transformer<R3, R4>
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>,
+  transformation4: Transformation<R3, R4>
 ): R4;
 export function pipe<IN, R1, R2, R3>(
   input: IN,
-  xd1: Transformer<IN, R1>,
-  xd2: Transformer<R1, R2>,
-  xd3: Transformer<R2, R3>
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>,
+  transformation3: Transformation<R2, R3>
 ): R3;
 export function pipe<IN, R1, R2>(
   input: IN,
-  xd1: Transformer<IN, R1>,
-  xd2: Transformer<R1, R2>
+  transformation1: Transformation<IN, R1>,
+  transformation2: Transformation<R1, R2>
 ): R2;
-export function pipe<IN, R1>(input: IN, xd1: Transformer<IN, R1>): R1;
+export function pipe<IN, R1>(input: IN, xd1: Transformation<IN, R1>): R1;
 export function pipe<IN>(input: IN): IN;
 export function pipe<IN, R>(
   input: IN,
-  ...Transducers: Transformer<any, any>[]
+  ...transformations: Transformation<any, any>[]
 ): R;
 
-export function pipe(input: any, ...transducers: any[]): any {
-  return compose(...transducers)(input);
+export function pipe(input: any, ...transformations: any[]): any {
+  let result = input;
+  for (const transformation of transformations) {
+    result = transformation(result);
+  }
+  return result;
 }
