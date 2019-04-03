db5205f104dde333db4c183599a4652be5d96a57
Artur Kulig <arturkulig@gmail.com>
2019-04-03 12:27:05 +0200

    add purging d.ts files to clean_up script

--- clean_up.ts
+++ clean_up.ts
@@ -12,10 +12,23 @@ const jss = glob
   .map(f => path.join(path.dirname(f), path.basename(f, ".js")))
   .filter(f => !f.includes("node_modules"));
 
+const dts = glob
+  .sync("packages/**/*.d.ts")
+  .map(f => path.join(path.dirname(f), path.basename(f, ".d.ts")))
+  .filter(f => !f.includes("node_modules"));
+
 jss
   .filter(file => tss.indexOf(file) >= 0)
   .map(l => `${l}.js`)
   .forEach(file => {
-    console.log(path.resolve(__dirname, file));
+    console.log("removing", path.resolve(__dirname, file));
+    fs.unlinkSync(path.resolve(__dirname, file));
+  });
+
+dts
+  .filter(file => tss.indexOf(file) >= 0)
+  .map(l => `${l}.d.ts`)
+  .forEach(file => {
+    console.log("removing", path.resolve(__dirname, file));
     fs.unlinkSync(path.resolve(__dirname, file));
   });
