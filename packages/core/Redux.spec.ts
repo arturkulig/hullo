02fc914d78d147a779fc2c818b4eaae84e2893c6
Artur Kulig <arturkulig@gmail.com>
2020-01-24 17:47:45 +0100

    core: enhance Redux

--- packages/core/Redux.spec.ts
+++ packages/core/Redux.spec.ts
@@ -1,21 +1,46 @@
 import { Redux } from "./Redux";
 
-it("Redux", async () => {
-  const redux = new Redux(
-    {
-      addNumber(state, data: number) {
-        return [...state, data];
+describe("Redux", () => {
+  it("overwrites state", async () => {
+    const redux = new Redux(
+      {
+        addNumber(state, data: number) {
+          return { state: [...state, data] };
+        },
+        async addString(state, data: string) {
+          return { state: [...state, data] };
+        }
       },
-      async addString(state, data: string) {
-        return [...state, data];
-      }
-    },
-    new Array<string | number>()
-  );
+      new Array<string | number>()
+    );
 
-  await redux.next({ type: "addNumber", data: 0 });
-  expect(redux.unwrap()).toEqual([0]);
+    await redux.next({ type: "addNumber", data: 0 });
+    expect(redux.unwrap()).toEqual([0]);
 
-  await redux.next({ type: "addString", data: "a" });
-  expect(redux.unwrap()).toEqual([0, "a"]);
+    await redux.next({ type: "addString", data: "a" });
+    expect(redux.unwrap()).toEqual([0, "a"]);
+  });
+
+  it("calls other actions", async () => {
+    const redux = new Redux(
+      {
+        addNumber(state, data: number) {
+          return {
+            state: [...state, data],
+            actions: [{ type: "addString", data: "ok" }]
+          };
+        },
+        async addString(state, data: string) {
+          return { state: [...state, data] };
+        }
+      },
+      new Array<string | number>()
+    );
+
+    await redux.next({ type: "addNumber", data: 0 });
+    expect(redux.unwrap()).toEqual([0, "ok"]);
+
+    await redux.next({ type: "addNumber", data: 1 });
+    expect(redux.unwrap()).toEqual([0, "ok", 1, "ok"]);
+  });
 });
--- packages/core/Redux.ts
+++ packages/core/Redux.ts
@@ -2,28 +2,37 @@ import { Observer } from "./Observable";
 import { Duplex } from "./Duplex";
 import { Atom } from "./Atom";
 
-type Reducers<STATE, ACTIONS> = {
+type ActionDescs = { [id: string]: any };
+
+type NowOrSometime<T> = Promise<T> | T;
+
+interface Effect<STATE, ACTIONS extends Record<string, any>> {
+  state?: STATE;
+  actions?: ACTION<ACTIONS>[];
+}
+
+type Reducers<STATE, ACTIONS extends ActionDescs> = {
   [a in keyof ACTIONS]: (
     state: STATE,
     action: ACTIONS[a]
-  ) => Promise<STATE> | STATE
+  ) => NowOrSometime<Effect<STATE, ACTIONS>>;
 };
 
-type ACTION<ACTIONS> = {
+type ACTION<ACTIONS extends ActionDescs> = {
   [action in keyof ACTIONS]: {
     type: action;
     data: ACTIONS[action];
-  }
+  };
 }[keyof ACTIONS];
 
-export class Redux<STATE, ACTIONS extends { [id: string]: any }> extends Duplex<
+export class Redux<STATE, ACTIONS extends ActionDescs> extends Duplex<
   ACTION<ACTIONS>,
   STATE
 > {
   private state$: Atom<STATE>;
 
   constructor(reducers: Reducers<STATE, ACTIONS>, init: STATE) {
-    const state$ = new Atom(init);
+    const state$ = new Atom<STATE>(init);
     const applier = new Reductor<STATE, ACTIONS>(reducers, state$);
     super(state$, applier);
     this.state$ = state$;
@@ -38,7 +47,8 @@ export class Redux<STATE, ACTIONS extends { [id: string]: any }> extends Duplex<
   }
 }
 
-class Reductor<STATE, ACTIONS> implements Observer<ACTION<ACTIONS>> {
+class Reductor<STATE, ACTIONS extends ActionDescs>
+  implements Observer<ACTION<ACTIONS>> {
   get closed() {
     return this.state$.closed;
   }
@@ -48,12 +58,23 @@ class Reductor<STATE, ACTIONS> implements Observer<ACTION<ACTIONS>> {
     private state$: Atom<STATE>
   ) {}
 
-  next(action: ACTION<ACTIONS>) {
-    return this.state$.update(state =>
-      this.reducers[action.type]
-        ? this.reducers[action.type](state, action.data)
-        : state
-    );
+  async next(firstAction: ACTION<ACTIONS>) {
+    const actions: ACTION<ACTIONS>[] = [firstAction];
+    while (actions.length) {
+      const action = actions.shift()!;
+      await this.state$.update(async (state: STATE) => {
+        if (this.reducers[action.type]) {
+          const res = await this.reducers[action.type](state, action.data);
+          if ("actions" in res && res.actions !== undefined) {
+            actions.push(...res.actions);
+          }
+          if ("state" in res && res.state !== undefined) {
+            return res.state;
+          }
+        }
+        return state;
+      });
+    }
   }
 
   complete() {
